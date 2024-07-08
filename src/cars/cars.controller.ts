73d3b9e203489dd8893647f5f0c20254c966bff0
LostoVayne <moises.barrera.dev@gmail.com>
2024-07-07 22:35:54 -0400

    Agrega un ValidatorPipe

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -8,6 +8,8 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
@@ -27,6 +29,7 @@ export class CarsController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createCar(@Body() createCarDto: CreateCarDto) {
     return createCarDto;
   }
--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,4 +1,9 @@
+import { IsString } from 'class-validator';
+
 export class CreateCarDto {
+  @IsString()
   readonly brand: string;
+
+  @IsString()
   readonly model: string;
 }
